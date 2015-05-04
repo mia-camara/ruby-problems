76825cc986e9ef4b888194ee2a7c385379995cea
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-04 17:20:09 +0800

    Reverse

---
+++ reverse.rb
@@ -0,0 +1,11 @@
+#### Reverse String
+## Write a function to reverse a string
+
+
+def reverse(string)
+	string.reverse
+end
+
+puts reverse("Mia")
+puts reverse("Hello World, this is a string I want to reverse")
+
