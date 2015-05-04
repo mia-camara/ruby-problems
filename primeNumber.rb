6f8df15aaa86c16cb0330a26151cd43bbad5bc0e
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-05 06:37:12 +0800

    PrimeNumber

--- primeNumber.rb
+++ primeNumber.rb
@@ -2,20 +2,19 @@
 ## Write a function to check if a number is a prime number
 
 
-def PrimeNumber(number)
-  if number%2 == 0
-  	puts "False, your number is not a Prime Number"
-  else
-  	puts "True, your number is a Prime Number"
+def PrimeNumber?(number)
+  for x in 2..(number - 1)
+  	if (number % x) == 0
+    	puts false
+  	else
+  		puts true 
+  	end
   end
-end
+ end
 
-PrimeNumber(5)
-PrimeNumber(6)
-PrimeNumber(1)
-PrimeNumber(7)
-PrimeNumber(13)
-PrimeNumber(10)
+PrimeNumber?(10)
+
+
 
 
 
